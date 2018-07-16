b9ea756942797e10539d3ed5b9b454309bd9db77
MaxXSoft <x@MaxXSoft.net>
2018-07-16 21:56:10 +0800

    (sim) added some files for simulating

---
+++ src/cpu/sim/RAM.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+
+`include "bus.v"
+
+module RAM(
+    input clk,
+    input rst,
+    input en,
+    input write_en,
+    input write_sel,
+    input [`ADDR_BUS] addr,
+    input [`DATA_BUS] data_in,
+    output [`DATA_BUS] data_out
+);
+
+    parameter kSubRamSize = 128;
+
+    reg[7:0] ram0[0:kSubRamSize - 1];
+    reg[7:0] ram1[0:kSubRamSize - 1];
+    reg[7:0] ram2[0:kSubRamSize - 1];
+    reg[7:0] ram3[0:kSubRamSize - 1];
+
+    assign data_out[7:0] = !rst && en && !write_en && !addr[31:9] &&
+            !addr[1:0] ? ram3[addr[31:2]] : 8'b0;
+    assign data_out[15: 8] = !rst && en && !write_en && !addr[31:9] &&
+            !addr[1:0] ? ram2[addr[31:2]] : 8'b0;
+    assign data_out[23:16] = !rst && en && !write_en && !addr[31:9] &&
+            !addr[1:0] ? ram1[addr[31:2]] : 8'b0;
+    assign data_out[31:24] = !rst && en && !write_en && !addr[31:9] &&
+            !addr[1:0] ? ram0[addr[31:2]] : 8'b0;
+
+    reg inner_en;
+    always @(posedge clk) begin
+        inner_en <= write_en && en && !rst;
+    end
+
+    always @(posedge inner_en) begin
+        if (inner_en && !addr[31:9] && !addr[1:0]) begin
+            if (write_sel[0]) ram3[addr[31:2]] <= data_in[7:0];
+            if (write_sel[1]) ram2[addr[31:2]] <= data_in[15:8];
+            if (write_sel[2]) ram1[addr[31:2]] <= data_in[23:16];
+            if (write_sel[3]) ram0[addr[31:2]] <= data_in[31:24];
+        end
+    end
+
+endmodule // RAM
--- src/cpu/sim/ROM.v
+++ src/cpu/sim/ROM.v
@@ -1,8 +1,23 @@
 `timescale 1ns / 1ps
 
+`include "bus.v"
+
 module ROM(
     input en,
-    input 
+    input [`ADDR_BUS] addr,
+    output [`INST_BUS] inst
 );
+
+    reg[7:0] rom[0:511];
+
+    initial begin
+        $readmemh("rom/inst.bin", rom);
+    end
+
+    // big endian storage
+    assign inst[7:0] = (en ? rom[addr + 3] : 8'b0);
+    assign inst[15:8] = (en ? rom[addr + 2] : 8'b0);
+    assign inst[23:16] = (en ? rom[addr + 1] : 8'b0);
+    assign inst[31:24] = (en ? rom[addr + 0] : 8'b0);
 
 endmodule // ROM
