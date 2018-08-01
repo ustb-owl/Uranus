b528852e34ba09982c879c4da6c2b163bc71ec69
MaxXSoft <x@MaxXSoft.net>
2018-08-01 13:38:04 +0800

    (autowire) updated autowire and fixed some bugs

---
+++ util/autowire/projscan.py
@@ -0,0 +1,131 @@
+import re, os
+from os.path import realpath
+
+class Util(object):
+    re_inc = re.compile(r'`include "(.*?)"')
+    re_macro_def = re.compile(r'`define ([A-Za-z_][A-Za-z0-9_]*)\s+(.*)')
+    re_macro = re.compile(r'(`[A-Za-z_][A-Za-z0-9_]*)')
+    re_comment = re.compile(r'//.*\n')
+    re_module = re.compile(r'module\s+([A-Za-z_][A-Za-z0-9_]*).*?\((.*?)\);')
+    re_ports = re.compile(r'\s?(input|output|inout)\s?(reg|wire)?\s?(\[.*?\])?\s?([A-Za-z_][A-Za-z0-9_]*)\s?,?\s?')
+    re_space = re.compile(r'\s\s+')
+
+    @staticmethod
+    def read_file(path):
+        try:
+            with open(path, 'r') as f:
+                return f.read()
+        except UnicodeDecodeError:
+            try:
+                with open(path, 'r', encoding='gb2312') as f:
+                    return f.read()
+            except UnicodeDecodeError:
+                return ''
+
+class VerilogFileObject(object):
+    def __init__(self):
+        self.path = ''
+        self.includes = []
+
+    def parse_includes(self):
+        base_path = self.path[:self.path.rfind('/') + 1]
+        content = Util.read_file(self.path)
+        self.includes.clear()
+        for inc in Util.re_inc.findall(content):
+            self.includes.append(realpath(base_path + inc))
+
+class VerilogModuleObject(object):
+    def __init__(self):
+        self.name = ''
+        self.port_def = ''
+        self.ports = {}
+
+    def parse(self, macro_man, includes):
+        defs = Util.re_ports.findall(self.port_def)
+        for i in defs:
+            bus = i[2]
+            if bus:
+                macros = Util.re_macro.findall(bus)
+                for m in macros:
+                    bus = bus.replace(m, macro_man.get(includes, m[1:]))
+                l = bus.strip('[]').split(':')
+                try:
+                    l[0] = eval(l[0])
+                    l[1] = eval(l[1])
+                except NameError:
+                    l = [0, 0]
+                self.ports[i[3]] = l
+            else:
+                self.ports[i[3]] = [0, 0]
+
+class VerilogMacroObject(object):
+    def __init__(self):
+        self.path = ''
+        self.macros = {}
+    
+    def parse(self):
+        content = Util.read_file(self.path)
+        for name, value in Util.re_macro_def.findall(content):
+            self.macros[name] = value
+
+class MacroManager(object):
+    def __init__(self):
+        self.macro_files = {}
+    
+    def scan_macros(self, includes):
+        for inc in includes:
+            if not self.macro_files.get(inc):
+                macro = VerilogMacroObject()
+                macro.path = inc
+                macro.parse()
+                self.macro_files[inc] = macro
+    
+    def get(self, includes, macro_name):
+        for inc in includes:
+            macro = self.macro_files[inc]
+            value = macro.macros.get(macro_name)
+            if value:
+                return value
+        return None
+
+class ProjectScanner(object):
+    def __init__(self):
+        self.files = []
+        self.modules = {}
+    
+    def scan(self, base_path):
+        if not os.path.exists(base_path) or not os.path.isdir(base_path):
+            return False
+        for f in os.listdir(base_path):
+            if not f.startswith('.'):
+                path = base_path
+                path += ('' if base_path.endswith('/') else '/') + f
+                if os.path.isfile(path):
+                    if f.endswith('.v'):
+                        file_obj = VerilogFileObject()
+                        file_obj.path = realpath(path)
+                        file_obj.parse_includes()
+                        self.files.append(file_obj)
+                elif not self.scan(path):
+                    return False
+        return True
+ 
+    def parse(self):
+        macro_man = MacroManager()
+        for obj in self.files:
+            macro_man.scan_macros(obj.includes)
+            content = Util.read_file(obj.path)
+            # remove comments and newlines
+            content = Util.re_comment.sub('\n', content).replace('\n', '')
+            for i in Util.re_module.findall(content):
+                mod_obj = VerilogModuleObject()
+                mod_obj.name = i[0]
+                mod_obj.port_def = Util.re_space.sub(' ', i[1])
+                mod_obj.parse(macro_man, obj.includes)
+                self.modules[i[0]] = mod_obj
+    
+    def get_bus(self, module_name, port_name):
+        try:
+            return self.modules[module_name].ports[port_name]
+        except KeyError:
+            return None
---
+++ util/autowire/sax.py
@@ -0,0 +1,95 @@
+from verilog_ast import *
+
+# define the SAX handler of block design file
+class BlockDesignSaxHandler(object):
+    def __init__(self):
+        self.__prefix = 'spirit:'
+        self.__tag_stack = []
+        self.__cur_attrs = {}
+        self.__file_ast = VerilogFileAST()
+        self.__cur_ast = None
+        self.scanner = None
+
+    def __tag_test(self, *args, back=False):
+        if back:
+            slice_pos = len(self.__tag_stack) - len(args)
+            return self.__tag_stack[slice_pos:] == list(args)
+        else:
+            return self.__tag_stack[:len(args)] == list(args)
+
+    def __attr_test(self, name, value=None):
+        name = self.__prefix + name
+        if not value:
+            return self.__cur_attrs.get(name)
+        else:
+            return self.__cur_attrs[name] == value
+
+    def start_element(self, name: str, attrs: dict):
+        tag_test = self.__tag_test
+        attr_test = self.__attr_test
+        if name.startswith(self.__prefix):
+            self.__tag_stack.append(name[len(self.__prefix):])
+            self.__cur_attrs = attrs
+            if tag_test('design', 'adHocConnections', 'adHocConnection'):
+                if tag_test('externalPortReference', back=True):
+                    self.__cur_ast.reference = attr_test('portRef')
+                if tag_test('internalPortReference', back=True):
+                    con_ast = VerilogIOConnectionAST()
+                    con_ast.port_name = attr_test('portRef')
+                    con_ast.wire_name = self.__cur_ast.name
+                    mod_ref = attr_test('componentRef')
+                    mod_ast = self.__file_ast.modules[mod_ref]
+                    mod_ast.connections.append(con_ast)
+                    bus = self.scanner.get_bus(mod_ast.module_name, con_ast.port_name)
+                    if self.__cur_ast.bus != bus:
+                        self.__cur_ast.bus = bus
+
+
+    def end_element(self, name: str):
+        tag_test = self.__tag_test
+        if name.startswith(self.__prefix):
+            if tag_test('component', 'model', 'ports', 'port', back=True):
+                self.__file_ast.ports[self.__cur_ast.name] = self.__cur_ast
+                self.__cur_ast = None
+            elif tag_test('design', 'componentInstances', 'componentInstance', back=True):
+                self.__file_ast.modules[self.__cur_ast.instance_name] = self.__cur_ast
+                self.__cur_ast = None
+            elif tag_test('design', 'adHocConnections', 'adHocConnection', back=True):
+                self.__file_ast.wires.append(self.__cur_ast)
+                self.__cur_ast = None
+            self.__tag_stack.pop()
+
+    def char_data(self, text: str):
+        tag_test = self.__tag_test
+        attr_test = self.__attr_test
+        try:
+            if tag_test('component', 'name'):
+                self.__file_ast.module_name = text
+            elif tag_test('component', 'model', 'ports', 'port'):
+                self.__cur_ast = self.__cur_ast or VerilogPortAST()
+                if tag_test('name', back=True):
+                    self.__cur_ast.name = text
+                elif tag_test('wire', 'direction', back=True):
+                    self.__cur_ast.direction = text
+                elif tag_test('wire', 'vector', 'left', back=True):
+                    self.__cur_ast.bus[0] = int(text)
+                elif tag_test('wire', 'vector', 'right', back=True):
+                    self.__cur_ast.bus[1] = int(text)
+            elif tag_test('design', 'componentInstances', 'componentInstance'):
+                self.__cur_ast = self.__cur_ast or VerilogModuleAST()
+                if tag_test('instanceName', back=True):
+                    self.__cur_ast.instance_name = text
+                elif tag_test('configurableElementValues',
+                        'configurableElementValue', back=True):
+                    if attr_test('referenceId', 'bd:referenceName'):
+                        self.__cur_ast.module_name = text
+            elif tag_test('design', 'adHocConnections', 'adHocConnection'):
+                self.__cur_ast = self.__cur_ast or VerilogWireAST()
+                if tag_test('name', back=True):
+                    self.__cur_ast.name = text.lower()
+        except IndexError:
+            pass
+    
+    def generate(self, bd_name):
+        content = self.__file_ast.generate()
+        return content.format(bd_name=bd_name)
