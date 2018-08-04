96dd1a231f0f77c0fa71e6392a86570db7549de4
MaxXSoft <x@MaxXSoft.net>
2018-08-04 10:07:32 +0800

    (CPU_tb) updated the testbench of CPU

--- src/cpu/sim/CPU_tb.v
+++ src/cpu/sim/CPU_tb.v
@@ -4,7 +4,50 @@ module CPU_tb();
 
     reg clk, rst;
 
-    Uranus cpu(clk, rst);
+    wire ram_en;
+    wire[3:0] ram_write_en;
+    wire[31:0] ram_addr;
+    wire[31:0] ram_write_data;
+    wire[31:0] ram_read_data;
+
+    wire rom_en;
+    // wire[3:0] rom_write_en;
+    wire[31:0] rom_addr;
+    // wire[31:0] rom_write_data;
+    wire[31:0] rom_read_data;
+
+    wire[31:0] debug_pc_addr;
+    wire[3:0] debug_reg_write_en;
+    wire[4:0] debug_reg_write_addr;
+    wire[31:0] debug_reg_write_data;
+
+    Uranus cpu(
+        .clk(clk),
+        .rst(rst),
+        .ram_en(ram_en),
+        .ram_write_en(ram_write_en),
+        .ram_addr(ram_addr),
+        .ram_write_data(ram_write_data),
+        .ram_read_data(ram_read_data),
+        .rom_en(rom_en),
+        // .rom_write_en(rom_write_en),
+        .rom_addr(rom_addr),
+        // .rom_write_data(rom_write_data),
+        .rom_read_data(rom_read_data),
+        .debug_pc_addr(debug_pc_addr),
+        .debug_reg_write_en(debug_reg_write_en),
+        .debug_reg_write_addr(debug_reg_write_addr),
+        .debug_reg_write_data(debug_reg_write_data)
+    );
+
+    RAM_tb ram(
+        clk, rst, ram_en, ram_write_en,
+        ram_addr, ram_write_data, ram_read_data
+    );
+
+    ROM_tb rom(
+        rom_en, rom_addr, rom_read_data
+    );
 
     initial begin
         clk = 0;
