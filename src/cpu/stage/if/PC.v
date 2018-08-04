bb81dca34322b1aa4ee4858f5f860649ca358e41
MaxXSoft <x@MaxXSoft.net>
2018-08-04 10:05:10 +0800

    (PC) modified the port definitions

--- src/cpu/stage/if/PC.v
+++ src/cpu/stage/if/PC.v
@@ -10,8 +10,13 @@ module PC(
     input [`ADDR_BUS] branch_addr,
     // output to ROM
     output reg rom_en,
-    output reg[`ADDR_BUS] addr
+    output [3:0] rom_write_en,
+    output reg[`ADDR_BUS] rom_addr,
+    output [`DATA_BUS] rom_write_data
 );
+
+    assign rom_wen = 0;
+    assign rom_write_data = 0;
 
     always @(posedge clk) begin
         rom_en <= rst;
@@ -19,13 +24,13 @@ module PC(
 
     always @(posedge clk) begin
         if (!rom_en) begin
-            addr <= 0;
+            rom_addr <= `ADDR_BUS_WIDTH'hbfc00000;
         end
         else if (branch_flag) begin
-            addr <= branch_addr;
+            rom_addr <= branch_addr;
         end
         else begin
-            addr <= addr + 4;
+            rom_addr <= rom_addr + 4;
         end
     end
 
