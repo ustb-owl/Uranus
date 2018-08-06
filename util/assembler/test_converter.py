2d4775f3a246a3f5e1dc1dafbd4381e9256264d6
jasper0819x <wanjixiang819@hotmail.com>
2018-08-06 15:48:18 +0800

    (assembler) complete CoeConverter

---
+++ util/assembler/test_converter.py
@@ -0,0 +1,47 @@
+import unittest
+from unittest import TestCase
+from .converter import CoeConverter
+from io import StringIO
+
+
+class TestCoeConverter(TestCase):
+    def test_empty(self):
+        f = StringIO()
+        converter = CoeConverter()
+        converter.do_convert(f)
+        f.seek(0)
+        self.assertEqual(
+            'memory_initialization_radix=16;\nmemory_initialization_vector=\n;',
+            f.read().lower())
+
+    def test_normal_1(self):
+        f = StringIO()
+        converter = CoeConverter()
+        converter.write(b'\x34\x01\x12\x34\xAC\x01\x00\x00\x34\x02\x12\x34\x34\x01\x00\x00')
+        converter.do_convert(f)
+        f.seek(0)
+        self.assertEqual(
+            'memory_initialization_radix=16;\nmemory_initialization_vector=\n'
+            '34011234\n'
+            'ac010000\n'
+            '34021234\n'
+            '34010000\n'
+            ';',
+            f.read().lower()
+        )
+
+    def test_normal_2(self):
+        f = StringIO()
+        converter = CoeConverter()
+        converter.write(b'\x34\x01\x12\x34\xAC\x01\x00\x00\x34\x02\x12\x34\x34\x01')
+        converter.do_convert(f)
+        f.seek(0)
+        self.assertEqual(
+            'memory_initialization_radix=16;\nmemory_initialization_vector=\n'
+            '34011234\n'
+            'ac010000\n'
+            '34021234\n'
+            '3401\n'
+            ';',
+            f.read().lower()
+        )
