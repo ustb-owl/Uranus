bd260d87438fb00b91b7a8a13499a55d3c451012
MaxXSoft <x@MaxXSoft.net>
2018-08-09 18:13:07 +0800

    (exception) added definitions about exception

---
+++ src/cpu/define/exception.v
@@ -0,0 +1,27 @@
+// exception entrance
+`define INIT_PC             32'hbfc00000
+`define EXC_PC              32'hbfc00380
+
+// exception type segment position
+`define EXC_TYPE_POS_INT    7:0
+`define EXC_TYPE_POS_IF     0
+`define EXC_TYPE_POS_RI     1
+`define EXC_TYPE_POS_OV     2
+`define EXC_TYPE_POS_TP     3
+`define EXC_TYPE_POS_BP     4
+`define EXC_TYPE_POS_SYS    5
+`define EXC_TYPE_POS_ADE    6   // NOTE: can be removed
+`define EXC_TYPE_POS_ERET   7
+
+// exception type definitions
+`define EXC_TYPE_NULL       8'h0
+`define EXC_TYPE_INT        8'h1
+`define EXC_TYPE_IF         8'h2
+`define EXC_TYPE_RI         8'h3
+`define EXC_TYPE_OV         8'h4
+`define EXC_TYPE_TP         8'h5
+`define EXC_TYPE_BP         8'h6
+`define EXC_TYPE_SYS        8'h7
+`define EXC_TYPE_ADEL       8'h8
+`define EXC_TYPE_ADES       8'h9
+`define EXC_TYPE_ERET       8'ha
